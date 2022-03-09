0afcd2b45da5422b76efc1c604001ef87379e3ec
schm00g <hk8igureu@relay.firefox.com>
2022-03-09 18:20:59 +0000

    mocha tests

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+import { assert } from "chai";
+import { descendingOrder } from '../src/solutions.js'
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(descendingOrder(0), 0)
+    assert.strictEqual(descendingOrder(1), 1)
+    assert.strictEqual(descendingOrder(111), 111)
+    assert.strictEqual(descendingOrder(15), 51)
+    assert.strictEqual(descendingOrder(1021), 2110)
+    assert.strictEqual(descendingOrder(123456789), 987654321)
+  })
+})
