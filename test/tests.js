9bde6ecff3520914cf1ea91371cc1fc830474c43
schm00g <hk8igureu@relay.firefox.com>
2022-03-09 22:02:19 +0000

    test assertions alpha positions

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,8 @@
 import { assert } from "chai";
-import { descendingOrder } from '../src/solutions.js'
+import { descendingOrder, alphabetPosition } from '../src/solutions.js'
 
 describe("Basic tests", () => {
-  it("Testing for fixed tests", () => {
+  it("should output descending order integer", () => {
     assert.strictEqual(descendingOrder(0), 0)
     assert.strictEqual(descendingOrder(1), 1)
     assert.strictEqual(descendingOrder(111), 111)
@@ -11,3 +11,10 @@ describe("Basic tests", () => {
     assert.strictEqual(descendingOrder(123456789), 987654321)
   })
 })
+
+describe("Tests", () => {
+  it("should output correct alphabetical positions", () => {
+    assert.strictEqual(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+    assert.strictEqual(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+  });
+});
