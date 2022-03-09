1a32a6dcd382144313d4c7d87b4df47c78d918f2
schm00g <hk8igureu@relay.firefox.com>
2022-03-09 22:01:45 +0000

    solution for alphabetical position of chars

--- src/solutions.js
+++ src/solutions.js
@@ -12,4 +12,51 @@ export function descendingOrder(n){
   return Number(arr.join(''));
 }
 
+/* 
+  In this kata you are required to, 
+  given a string, replace every letter 
+  with its position in the alphabet.
+*/
 
+export function alphabetPosition(text) {
+  const position = {
+    a: "1",
+    b: "2",
+    c: "3",
+    d: "4",
+    e: "5",
+    f: "6",
+    g: "7",
+    h: "8",
+    i: "9",
+    j: "10",
+    k: "11",
+    l: "12",
+    m: "13",
+    n: "14",
+    o: "15",
+    p: "16",
+    q: "17",
+    r: "18",
+    s: "19",
+    t: "20",
+    u: "21",
+    v: "22",
+    w: "23",
+    x: "24",
+    y: "25",
+    z: "26"
+  }
+  
+  let result = "";
+  
+  for(let i = 0; i <= text.length; i++){
+    if((/[a-zA-Z]/).test(text.charAt(i))){
+      result = result.concat(position[String(text.charAt(i)).toLowerCase()]);
+      if(i < text.length - 2){
+        result = result.concat(" ");
+      }
+    }
+  };
+  return result;
+}
