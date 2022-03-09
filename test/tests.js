1dce22dcb59dbdd95655d9433d466ed992784263
schm00g <hk8igureu@relay.firefox.com>
2022-03-09 22:21:14 +0000

    perfect square test

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { descendingOrder, alphabetPosition } from '../src/solutions.js'
+import { descendingOrder, alphabetPosition, isSquare } from '../src/solutions.js'
 
 describe("Basic tests", () => {
   it("should output descending order integer", () => {
@@ -16,5 +16,16 @@ describe("Tests", () => {
   it("should output correct alphabetical positions", () => {
     assert.strictEqual(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
     assert.strictEqual(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+  });
+});
+
+describe("isSquare", function(){
+  it("should check if numbers are perfect squares", function(){
+    Test.assertEquals(isSquare(-1), false, "-1: Negative numbers cannot be square numbers");
+    Test.assertEquals(isSquare(0), true, "0 is a square number (0 * 0)");
+    Test.assertEquals(isSquare(3), false, "3 is not a square number");
+    Test.assertEquals(isSquare(4), true, "4 is a square number (2 * 2)");
+    Test.assertEquals(isSquare(25), true, "25 is a square number (5 * 5)");
+    Test.assertEquals(isSquare(26), false, "26 is not a square number");
   });
 });
